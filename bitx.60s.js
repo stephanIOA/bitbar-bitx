b9166d0a9b2c17eeb16f1f37a0988e0cbccb87e2
Stephan Bothma <stephan@io.co.za>
2016-01-07 15:15:53 +0200

    Add plugin

---
+++ bitx.60s.js
@@ -0,0 +1,22 @@
+#!/usr/local/bin/node
+
+var fs = require('fs');
+var https = require('https');
+
+https.get('https://api.mybitx.com/api/1/ticker?pair=XBTZAR', function(res) {
+  var body = '';
+  res.on('data', function(data) {
+    body += data;
+  }).on('error', function(err) {
+    console.log('...')
+  }).on('end', function() {
+    printStats(body);
+  });
+})
+
+function printStats(body) {
+  console.log('R ' + body.bid + '/B⃦| color=#333333');
+  console.log('---');
+  console.log('Ask: R', body.ask, '| color=#cc0000');
+  console.log('Bid: R', body.bid, '| color=#00cc00');
+}
